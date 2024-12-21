6bd789a58c15008810f764aa741149d021973edf
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-21 08:23:32 +0800

    initial commit: set up Django blog project with posts app, models, views, serializers, and migrations

---
+++ posts/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create_user(
+            username='kiyo', password='rmvilla28')
+        testuser1.save()
+
+        # Create a blog post
+        test_post = Post.objects.create(
+            author=testuser1, title='Blog title', body='Body content...')
+        test_post.save()
+
+def test_blog_content(self):
+    post = Post.objects.get(id=1)
+    author = f'{post.author}'
+    title = f'{post.title}'
+    body = f'{post.body}'
+    self.assertEqual(author, 'kiyo')
+    self.assertEqual(title, 'Blog title')
+    self.assertEqual(body, 'Body content...')
---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
